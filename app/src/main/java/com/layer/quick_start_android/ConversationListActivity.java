0e77542cbbb16e0427cfe3693beee6e3cd9c0818
rkanderson <shnoutyanderson@gmail.com>
2015-07-21 22:06:44 -0700

    custom layout for counselor bar item

--- app/src/main/java/com/layer/quick_start_android/ConversationListActivity.java
+++ app/src/main/java/com/layer/quick_start_android/ConversationListActivity.java
@@ -16,6 +16,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
